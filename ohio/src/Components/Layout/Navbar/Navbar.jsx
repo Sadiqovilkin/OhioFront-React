e07d82d01a86d3dadeeb22a6bc72d7475b37a664
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-02-09 16:23:46 +0400

    #Footer Added

--- ohio/src/Components/Layout/Navbar/Navbar.jsx
+++ ohio/src/Components/Layout/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Navbar/Navbar.scss";
+import "./Navbar.scss";
 import { RiMenu2Fill } from "react-icons/ri";
 import { AiOutlinePlus } from "react-icons/ai";
 import { Link } from "react-router-dom";
