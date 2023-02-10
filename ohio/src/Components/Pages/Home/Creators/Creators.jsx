e18de8a344f94a92d297ff0ea85db1223a15cd43
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-02-10 14:06:00 +0400

    #Finished Creators Section

--- ohio/src/Components/Pages/Home/Creators/Creators.jsx
+++ ohio/src/Components/Pages/Home/Creators/Creators.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import "./Creators.scss";
 const Creators = () => {
-  return <div>Creators</div>;
+  return (
+    <section id="creators">
+      <div className="container">
+        <div className="row justify-content-between">
+          <div className="col-lg-4 ">
+            <div className="left_side">
+              <span>We are proud of the works we’ve done.</span>
+              <h2>We’re design creators.</h2>
+              <p>More Projects</p>
+            </div>
+          </div>
+
+          <div className="col-lg-7">
+            <div className="crt_img">
+              <img
+                src="https://colabrio.ams3.cdn.digitaloceanspaces.com/ohio-stage-demo-1/oh__demo1__1.png"
+                alt=""
+              />
+              <div className="crt_content">
+                <span>Featured project:</span>
+                <h4>Unique Bowls From the South Pacific.</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Creators;
