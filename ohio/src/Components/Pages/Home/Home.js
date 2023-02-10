e34c44e35c0bb78db8759650f57849ec9d07f596
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-02-10 13:43:45 +0400

    #Home Branch Added

--- ohio/src/Components/Pages/Home/Home.js
+++ ohio/src/Components/Pages/Home/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Cart from "./Carts/Cart";
+import Creators from "./Creators/Creators";
+import Ideas from "./Ideas/Ideas";
+import Product from "./ProductImg/Product";
+import Project from "./Projects/Project";
+import Tecnology from "./Tecnology/Tecnology";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <Creators />
+      <Ideas />
+      <Product />
+      <Tecnology />
+      <Cart />
+      <Project />
+    </main>
+  );
 };
 
 export default Home;
