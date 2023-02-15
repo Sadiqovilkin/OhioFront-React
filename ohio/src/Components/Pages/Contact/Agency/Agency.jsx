001733a4ab38227106cc3d6f02a4894bf9814aae
Sadiqovilkin <sadiqovilkin068@gmail.com>
2023-02-15 15:37:15 +0400

    #Agency sections Finished

--- ohio/src/Components/Pages/Contact/Agency/Agency.jsx
+++ ohio/src/Components/Pages/Contact/Agency/Agency.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "./Agency.scss";
 const Agency = () => {
-  return <div>Agency</div>;
+  return (
+    <section id="agency">
+      <div className="agency_content">
+        <h1>Contact: Agency</h1>
+        <p>Leave us a little info, and we’ll be in touch.</p>
+        <a href="#">Send Us an Email</a>
+      </div>
+    </section>
+  );
 };
 
 export default Agency;
--- ohio/src/Components/Pages/Contact/Contact.js
+++ ohio/src/Components/Pages/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Agency from "./Agency/Agency";
 import Forms from "./Forms/Forms";
-import Map from "./Map/map";
+import Map from "./Map/Map";
 
 const Contact = () => {
   return (
